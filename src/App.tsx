c91d4c3fe3d89a6fed3b1dcb235a3d2e2e02d0b3
Kim Son <sonngo81.99@gmail.com>
2025-06-18 13:17:20 +0700

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import "./App.css";
-import ThemeSwitch from "./components/ThemeSwitch";
 
 function App() {
+	useGSAP(() => {
+		gsap
+			.timeline()
+			.to(".the-light", {
+				opacity: "100%",
+				ease: "bounce.inOut",
+				duration: 1,
+			})
+			.to(".the-light", {
+				opacity: "0%",
+				ease: "bounce.inOut",
+				duration: 0.5,
+			})
+			.repeat(2)
+			.to(".the-light", {
+				opacity: "100%",
+				ease: "bounce.inOut",
+				duration: 1,
+			})
+			.delay(0.1);
+	});
 	return (
 		<>
-			<div className="dark:bg-black bg-white h-[100vh] w-[100vw] p-4">
-				<ThemeSwitch />
+			<div className="dark:bg-black bg-white h-[100vh] w-[100vw] pt-4 pb-4">
+				<div className="relative">
+					<div
+						className="the-light absolute -top-80 -right-96 opacity-0
+					 border-l-[100vh] border-r-[100vh] border-b-[150vh] border-b-amber-100 border-l-transparent border-r-transparent rotate-[45deg]"
+					></div>
+				</div>
 			</div>
 		</>
 	);
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,57 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP, DrawSVGPlugin, MotionPathPlugin, ScrollTrigger);
+
+const Timeline = () => {
+	const ref = useRef<HTMLDivElement>(null);
+
+	useGSAP(
+		() => {
+			gsap
+				.timeline({
+					scrollTrigger: {
+						trigger: "#svg-stage",
+						scrub: true,
+						start: "top top",
+						end: "bottom center",
+					},
+				})
+				.from(".theLine", { drawSVG: 0 }, 0)
+				.to(".end", { drawSVG: 1 }, 0)
+				.to(
+					".circle",
+					{
+						motionPath: {
+							path: ".theLine",
+							align: ".theLine",
+							alignOrigin: [0.5, 0.5],
+						},
+					},
+					0
+				);
+		},
+		{ scope: ref }
+	);
+	return (
+		<div className="relative" ref={ref}>
+			<svg id="svg-stage" viewBox="0 0 1000 3000">
+				<path
+					className="theLine"
+					d="M 276 -64 Q 1333 528 466 1207 Q -425 1878 572 2615"
+					fill="none"
+					stroke="white"
+					strokeWidth="10px"
+				/>
+				<circle className="circle" r="10" fill="white" cx="100" cy="20" />
+				<path className="end" />
+			</svg>
+			<div className="text-white text-base absolute top-[20%] left-[10%]">
+				text
+			</div>
+		</div>
+	);
+};
+
+export default Timeline;
